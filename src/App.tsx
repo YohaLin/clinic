10b396770603b7ec0fea3ee2f25061d259d0ddbd
YohaLin <kate1995y@gmail.com>
2024-06-06 23:45:24 +0800

    bugfix: fix 404 when url is /

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import "./styles/index.css";
 import Appointment from "./pages/Appointment/Appointment";
 import DefaultLayout from "./layouts/DefaultLayout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [showHamburgerModal, setShowHamburgerModal] = useState(false);
+  const navigate = useNavigate();
+
+  // "/" 直接取代成 "/zh-TW/"
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      navigate("/zh-TW/", { replace: true });
+    }
+  }, [navigate]);
 
   return (
     <Routes>
